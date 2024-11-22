0b3e324ae37ce58cae199f099450961677022eba
piperhuntress <jasminetumulak@gmail.com>
2024-11-22 17:37:48 +0400

    added location

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -9,7 +9,9 @@ import { useEffect } from "react";
 import Location from "./Location";
 
 const Home = () => {
-  const email = useSelector((state) => state.users.user.email);
+  const email = useSelector((state) =>
+    state.users && state.users.user ? state.users.user.email : ""
+  );
   const navigate = useNavigate();
 
   useEffect(() => {
